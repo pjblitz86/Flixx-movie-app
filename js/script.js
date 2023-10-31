794ddc4abdf4987159c02c5b1ac1926ca38bba2a
pjblitz86 <paulius.juknis@gmail.com>
2023-10-31 10:25:11 +0200

    GET and display movie details page from API

--- js/script.js
+++ js/script.js
@@ -68,6 +68,75 @@ async function displayPopularShows() {
   });
 }
 
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement('div');
+  div.innerHTML = `
+        <div class="details-top">
+          ${
+            movie.poster_path
+              ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+              : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+            `
+          }   
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${movie.production_companies
+              .map(
+                (company) => `
+              <span>${company.name}</span>
+            `
+              )
+              .join('')}
+          </div>
+        </div>
+    `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 async function fetchAPIData(endpoint) {
   const API_KEY = '3a6c8a71d0c54c451e4ee05c7fe43564';
   const API_URL = 'https://api.themoviedb.org/3/';
@@ -100,6 +169,10 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function init() {
   switch (global.currentPage) {
     case '/':
@@ -109,8 +182,8 @@ function init() {
     case '/shows.html':
       displayPopularShows();
       break;
-    case '/movie/details.html':
-      console.log('Movie Details');
+    case '/movie-details.html':
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
